25c86431fbaf96a2c884962129ba6b0d04558145
MatheusLm04 <95518646+jmatheuslima@users.noreply.github.com>
2023-12-15 18:41:53 +0000

    adicionar `prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ function home() {
 }
 
 function teste() {
-  console.log ("teste");
+  console.log("teste");
 }
 
-function teste2 () {
-      console.log('indentação errada')
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default home;
