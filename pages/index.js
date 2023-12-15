32b9782b015cd03d878d651a13111a1ef8f15387
MatheusLm04 <95518646+jmatheuslima@users.noreply.github.com>
2023-12-15 18:18:36 +0000

    adicionar scrips `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function home() {
-    return <h1>Olá Tudo Bem 🤣</h1>
+  return <h1>Olá Tudo Bem 🤣</h1>;
+}
+
+function teste() {
+  console.log ("teste");
+}
+
+function teste2 () {
+      console.log('indentação errada')
 }
 
 export default home;
