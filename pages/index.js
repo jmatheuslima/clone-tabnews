cbcd5a9b84e621503c32d703a42a5c9f5f7db92a
MatheusLm04 <95518646+jmatheuslima@users.noreply.github.com>
2023-12-29 18:46:38 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function home() {
   return <h1>Olá Tudo Bem 🤣</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default home;
